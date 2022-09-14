29b1ecdae00e106b974f0d782a681870cbbfcae4
Artem <shvetsartem7@gmail.com>
2022-09-14 17:37:51 -0400

    First Extent Commit

---
+++ MavenExtentPro/src/test/java/com/test/SampleTest.java
@@ -0,0 +1,21 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void testCase_1() {
+		System.out.println("This is Test Case 1");
+	}
+	
+	@Test
+	public void testCase_2() {
+		System.out.println("This is Test Case 2");
+	}
+	
+	@Test
+	public void testCase_3() {
+		System.out.println("This is Test Case 3");
+	}
+}
